55dea1c39cfba542cc83b670fb7f6083ade6a243
lerkasan <lerkasan@gmail.com>
2017-09-12 16:17:56 +0300

    changed vagrant provisioning to run ansible playbook

--- vagrant/ansible/ansible_init.sh
+++ vagrant/ansible/ansible_init.sh
@@ -4,5 +4,5 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
 sudo apt-get -y install python python-pip
-
-# ansible-playbook playbooks/vagrant-playbook-init.yml --vault-password-file=conf/.vault_pass
+pip install --upgrade pip
+sudo pip install ansible
--- vagrant/ansible/ansible_startup.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-ansible-playbook playbooks/vagrant-playbook-startup.yml --vault-password-file=conf/.vault_pass
