9531026c9fd2d7f49f2f841be0c690ac0514a519
lerkasan <lerkasan@gmail.com>
2017-10-09 17:07:34 +0300

    experiments with checkout and emailext steps

--- jenkins/job-dsl/tomcat/jobdsl-postbuild-step.sh
+++ jenkins/job-dsl/tomcat/jobdsl-postbuild-step.sh
@@ -8,6 +8,8 @@ export AWS_DEFAULT_REGION="us-west-2"
 export AWS_SECRET_ACCESS_KEY=`get_from_parameter_store "SECRET_ACCESS_KEY"`
 export AWS_ACCESS_KEY_ID=`get_from_parameter_store "ACCESS_KEY_ID"`
 
+BUCKET_NAME="ansible-demo1"
 ARTIFACT_FILENAME=`ls ${WORKSPACE}/target | grep war | grep -v original`
+
 aws s3 cp ${WORKSPACE}/target/${ARTIFACT_FILENAME} s3://${BUCKET_NAME}/${ARTIFACT_FILENAME}
 mv ${WORKSPACE}/target/${ARTIFACT_FILENAME} ${WORKSPACE}/target/ROOT.war
