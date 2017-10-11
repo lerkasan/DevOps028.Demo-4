9587fa329ac2a766b757630dc56e4f85ed899b8e
lerkasan <lerkasan@gmail.com>
2017-10-11 20:58:56 +0300

    final fixes

--- jenkins/job-dsl/check-webapp-response.sh
+++ jenkins/job-dsl/check-webapp-response.sh
@@ -20,12 +20,12 @@ export ELB_PORT=`echo ${ELB_INFO} | grep LISTENERS | awk '{print $3}'`
 echo "ELB endpoint: ${ELB_HOST}:${ELB_PORT}"
 
 # Wait for Spring Boot application to launch properly
-echo "Waiting 30 seconds for Spring Boot application to launch properly before checking connectivity to webapp load balancer ..."
-sleep 30
+echo "Waiting 60 seconds for Spring Boot application to launch properly before checking connectivity to webapp load balancer ..."
+sleep 60
 
 # Check connectivity to webapp loadbalancer
 echo "Checking connectivity to webapp load balancer ..."
-HTTP_CODE=`curl -s -o /dev/null -w "%{http_code}" "http://${ELB_HOST}:${ELB_PORT}"`
+HTTP_CODE=`curl -s -o /dev/null -w "%{http_code}" "http://${ELB_HOST}:${ELB_PORT}/login"`
 if [[ ${HTTP_CODE} > 399 ]]; then
 	echo "HTTP_RESPONSE_CODE = ${HTTP_CODE}"
 	exit 1
