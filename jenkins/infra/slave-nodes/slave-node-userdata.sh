858c992892995899f6a6e2a2b1ee1984fdb2786a
lerkasan <lerkasan@gmail.com>
2017-10-16 01:18:29 +0300

    small fix

--- jenkins/infra/slave-nodes/slave-node-userdata.sh
+++ jenkins/infra/slave-nodes/slave-node-userdata.sh
@@ -45,8 +45,8 @@ echo "PubkeyAuthentication yes" | sudo tee --append /etc/ssh/sshd_config
 sudo service sshd restart
 
 #Download and install Terraform
-wget https://releases.hashicorp.com/terraform/0.10.7/terraform_0.10.7_linux_amd64.zip
-unzip terraform_0.10.7_linux_amd64.zip
+cd "/home/${OS_USERNAME}" && wget https://releases.hashicorp.com/terraform/0.10.7/terraform_0.10.7_linux_amd64.zip
+cd "/home/${OS_USERNAME}" && unzip terraform_0.10.7_linux_amd64.zip
 export PATH="/home/ec2-user:${PATH}"
 
 # Download and install JDK
