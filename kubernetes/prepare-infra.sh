c00953193557cd15aad15bcb5b7751d2c10003dc
lerkasan <lerkasan@gmail.com>
2017-11-02 14:19:55 +0200

    some debug added

--- kubernetes/prepare-infra.sh
+++ kubernetes/prepare-infra.sh
@@ -14,13 +14,18 @@ export CLUSTER_NAME=`get_from_parameter_store "demo3_cluster_name"`
 export KOPS_STATE_STORE=`get_from_parameter_store "demo3_kops_state_bucket"`
 
 # kops create cluster --zones us-west-2a ${CLUSTER_NAME} -f kops-infra.yaml
+echo "Creating cluster ..."
+whoami
 kops create cluster --zones us-west-2a ${CLUSTER_NAME} --master-size=t2.micro --node-size=t2.micro \
                                                        --master-volume-size=8 --node-volume-size=8
 kops update cluster ${CLUSTER_NAME} --yes
+echo "Adding dashboard ..."
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/kops/master/addons/kubernetes-dashboard/v1.7.1.yaml
 #kubectl apply -f pod.yaml
+echo "Deploying database ..."
 kubectl apply -f database.yaml
 sleep 20
+echo "Deploying web application ..."
 kubectl apply -f webapp.yaml
 #kubectl run --image=370535134506.dkr.ecr.us-west-2.amazonaws.com/samsara:latest demo3-samsara-app --port=9000 --replicas=2
 #kubectl expose deployment demo3-samsara-app --port=9000 --type=LoadBalancer
