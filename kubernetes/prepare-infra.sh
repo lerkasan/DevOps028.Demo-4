6eb410b611989ed62bcdce85e6ff0d891cb3f087
lerkasan <lerkasan@gmail.com>
2017-11-02 14:24:49 +0200

    some debug added

--- kubernetes/prepare-infra.sh
+++ kubernetes/prepare-infra.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+set -e
 # Requires one argument:
 # string $1 - name of parameter in EC2 parameter store
 function get_from_parameter_store {
@@ -15,7 +15,6 @@ export KOPS_STATE_STORE=`get_from_parameter_store "demo3_kops_state_bucket"`
 
 # kops create cluster --zones us-west-2a ${CLUSTER_NAME} -f kops-infra.yaml
 echo "Creating cluster ..."
-whoami
 kops create cluster --zones us-west-2a ${CLUSTER_NAME} --master-size=t2.micro --node-size=t2.micro \
                                                        --master-volume-size=8 --node-volume-size=8
 kops update cluster ${CLUSTER_NAME} --yes
@@ -23,10 +22,10 @@ echo "Adding dashboard ..."
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/kops/master/addons/kubernetes-dashboard/v1.7.1.yaml
 #kubectl apply -f pod.yaml
 echo "Deploying database ..."
-kubectl apply -f database.yaml
+kubectl apply -f kubernetes/database.yaml
 sleep 20
 echo "Deploying web application ..."
-kubectl apply -f webapp.yaml
+kubectl apply -f kubernetes/webapp.yaml
 #kubectl run --image=370535134506.dkr.ecr.us-west-2.amazonaws.com/samsara:latest demo3-samsara-app --port=9000 --replicas=2
 #kubectl expose deployment demo3-samsara-app --port=9000 --type=LoadBalancer
 # kubectl proxy
