d6bbe4e252d5e0f15bf34f332cdd0ad9abd63798
lerkasan <lerkasan@gmail.com>
2017-11-02 20:28:49 +0200

    misc changes

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+# Requires one argument:
+# string $1 - name of parameter in EC2 parameter store
+function get_from_parameter_store {
+    aws ssm get-parameter --name $1 --with-decryption --output text | awk '{print $4}'
+}
+
+export AWS_DEFAULT_REGION="us-west-2"
+export AWS_SECRET_ACCESS_KEY=`get_from_parameter_store "jenkins_secret_access_key"`
+export AWS_ACCESS_KEY_ID=`get_from_parameter_store "jenkins_access_key_id"`
+
 kubectl apply -f kubernetes/webapp.yaml
 sleep 20
 kubectl apply -f kubernetes/pod.yaml
