3c38ce58fe3158d8e6eec2118cc58a1ba5222d82
lerkasan <lerkasan@gmail.com>
2017-11-04 19:43:36 +0200

    minor fix at relative path to shell script

--- jenkins/pipeline/prepare-infra.sh
+++ jenkins/pipeline/prepare-infra.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-chmod +x "${WORKSPACE}/terraform/*.sh"
 cd "${WORKSPACE}/terraform"
+chmod +x ./*.sh
 #/home/ec2-user/terraform init -backend-config=backend.tf -backend=true -force-copy -get=true -input=false
 /home/ec2-user/terraform init -backend-config="bucket=${TF_VAR_bucket_name}" -backend-config="key=terraform/terraform.tfstate" \
                               -backend-config="region=${AWS_DEFAULT_REGION}" -backend=true -force-copy -get=true -input=false
