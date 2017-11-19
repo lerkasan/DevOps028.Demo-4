07f69412f85695b8f35af18048c9374b5905f035
lerkasan <lerkasan@gmail.com>
2017-11-19 22:25:52 +0200

    minor refactoring

--- kubernetes/create_clusters.sh
+++ kubernetes/create_clusters.sh
@@ -4,9 +4,13 @@ export AWS_DEFAULT_REGION="us-west-2"
 # export AWS_SECRET_ACCESS_KEY=`get_from_parameter_store "jenkins_secret_access_key"`
 # export AWS_ACCESS_KEY_ID=`get_from_parameter_store "jenkins_access_key_id"`
 
-PATH_TO_TLS="/etc/letsencrypt/live/registry.lerkasan.de"
-DNS_RECORDS_FILE="dns_records.json"
+JENKINS_REGISTRY_CLUSTER="jenkins-registry"
+REGISTRY_URL="registry.lerkasan.de"
+REGISTRY_DNS_RECORDS_FILE="registry_dns_records.json"
+JENKINS_SAMSARA_DNS_RECORDS_FILE="jenkins_samsara_dns_records.json"
 HOSTED_ZONE_ID="ZZ3Z055672IF0"
+PATH_TO_TLS="/etc/letsencrypt/live/registry.lerkasan.de"
+
 
 function get_loadbalancer_name {
     kubectl describe svc $1 | grep Ingress | awk '{print $3}' | awk -F "-" '{print $1}'
@@ -54,56 +58,16 @@ function create_cluster {
     cat ~/.kube/config
 }
 
-create_cluster jenkins-registry
+
+create_cluster ${JENKINS_REGISTRY_CLUSTER}
+aws iam  attach-role-policy --role-name "nodes.${JENKINS_REGISTRY_CLUSTER}.lerkasan.de" --policy-arn arn:aws:iam::370535134506:policy/jenkins-nodes-kops
 kubectl create namespace registry
 kubectl create namespace jenkins
 kubectl apply -f "registry-deployment.yaml" --namespace=registry
-REGISTRY_URL=registry.lerkasan.de
-
-# REGISTRY_URL="`kubectl describe svc registry | grep Ingress | awk '{print $3}'`:5000"
-## REGISTRY_URL=`kubectl describe svc registry | grep Ingress | awk '{print $3}'`
-## sed -i "s/{{registry_url}}/${REGISTRY_URL}/g" *.yaml
-## kubectl apply -f "registry-deployment.yaml"
-
-docker build -t jenkins-slave:latest -f jenkins/Dockerfile.jenkins_slave jenkins
-docker tag jenkins-slave:latest "${REGISTRY_URL}/jenkins-slave:latest"
-docker push "${REGISTRY_URL}/jenkins-slave:latest"
-
-docker build -t jenkins-master:latest -f jenkins/Dockerfile.jenkins_master jenkins
-docker tag jenkins-master:latest "${REGISTRY_URL}/jenkins-master:latest"
-docker push "${REGISTRY_URL}/jenkins-master:latest"
-
-kubectl apply -f "jenkins-deployment.yaml" --namespace=jenkins
-aws iam  attach-role-policy --role-name nodes.jenkins-cluster.k8s.local --policy-arn arn:aws:iam::370535134506:policy/jenkins-nodes-kops
-
-create_cluster samsara
-kubectl create namespace samsara
-kubectl create secret generic dbuser-pass --from-literal=password=mysecretpassword
-kubectl apply -f "database-deployment.yaml" --namespace=samsara
-kubectl apply -f "samsara-deployment.yaml" --namespace=samsara
-kubectl apply -f "samsara-pod.yaml" --namespace=samsara
-
-JENKINS_ELB_NAME=`get_loadbalancer_name jenkins`
-JENKINS_ELB_DNS=`get_loadbalancer_dns jenkins`
-JENKINS_ELB_ZONE_ID=`get_loadbalancer_zoneid ${JENKINS_ELB_NAME}`
 
 REGISTRY_ELB_NAME=`get_loadbalancer_name registry`
 REGISTRY_ELB_DNS=`get_loadbalancer_dns registry`
 REGISTRY_ELB_ZONE_ID=`get_loadbalancer_zoneid ${REGISTRY_ELB_NAME}`
-
-SAMSARA_ELB_NAME=`get_loadbalancer_name samsara`
-SAMSARA_ELB_DNS=`get_loadbalancer_dns samsara`
-SAMSARA_ELB_ZONE_ID=`get_loadbalancer_zoneid ${SAMSARA_ELB_NAME}`
-
-sed "s/%JENKINS_ELB_DNS%/${JENKINS_ELB_DNS}/g" "template_${DNS_RECORDS_FILE}" |
-    sed "s/%JENKINS_ELB_ZONE_ID%/${JENKINS_ELB_ZONE_ID}/g" |
-    sed "s/%REGISTRY_ELB_DNS%/${REGISTRY_ELB_DNS}/g" |
-    sed "s/%REGISTRY_ELB_ZONE_ID%/${REGISTRY_ELB_ZONE_ID}/g" |
-    sed "s/%SAMSARA_ELB_DNS%/${SAMSARA_ELB_DNS}/g" |
-    sed "s/%SAMSARA_ELB_ZONE_ID%/${SAMSARA_ELB_ZONE_ID}/g" > ${DNS_RECORDS_FILE}
-
-aws route53 change-resource-record-sets --hosted-zone-id ${HOSTED_ZONE_ID} --change-batch "file://${DNS_RECORDS_FILE}"
-# aws route53 get-change --id <place-change-id-here>
 
 REGISTRY_ELB_EC2_INSTANCES=`aws elb describe-load-balancers --load-balancer-name ${REGISTRY_ELB_NAME} \
     --output text --query 'LoadBalancerDescriptions[*].Instances[*].InstanceId'`
@@ -116,3 +80,41 @@ for INSTANCE_ID in ${REGISTRY_ELB_EC2_INSTANCES}; do
     sudo scp -q -i ~/.ssh/id_rsa "${PATH_TO_TLS}/privkey.pem" "admin@${EC2_HOST}:/home/admin/server.key"
     sudo scp -q -i ~/.ssh/id_rsa "${PATH_TO_TLS}/fullchain.pem" "admin@${EC2_HOST}:/home/admin/server.crt"
 done
+
+sed "s/%REGISTRY_ELB_DNS%/${REGISTRY_ELB_DNS}/g" "template_${REGISTRY_DNS_RECORDS_FILE}" |
+    sed "s/%REGISTRY_ELB_ZONE_ID%/${REGISTRY_ELB_ZONE_ID}/g" > ${REGISTRY_DNS_RECORDS_FILE}
+aws route53 change-resource-record-sets --hosted-zone-id ${HOSTED_ZONE_ID} --change-batch "file://${REGISTRY_DNS_RECORDS_FILE}"
+
+docker build -t jenkins-slave:latest -f jenkins/Dockerfile.jenkins_slave jenkins
+docker tag jenkins-slave:latest "${REGISTRY_URL}/jenkins-slave:latest"
+docker push "${REGISTRY_URL}/jenkins-slave:latest"
+
+docker build -t jenkins-master:latest -f jenkins/Dockerfile.jenkins_master jenkins
+docker tag jenkins-master:latest "${REGISTRY_URL}/jenkins-master:latest"
+docker push "${REGISTRY_URL}/jenkins-master:latest"
+
+kubectl apply -f "jenkins-deployment.yaml" --namespace=jenkins
+
+
+create_cluster samsara
+kubectl create namespace samsara
+kubectl create secret generic dbuser-pass --from-literal=password=mysecretpassword
+kubectl apply -f "database-deployment.yaml" --namespace=samsara
+kubectl apply -f "samsara-deployment.yaml" --namespace=samsara
+kubectl apply -f "samsara-pod.yaml" --namespace=samsara
+
+JENKINS_ELB_NAME=`get_loadbalancer_name jenkins`
+JENKINS_ELB_DNS=`get_loadbalancer_dns jenkins`
+JENKINS_ELB_ZONE_ID=`get_loadbalancer_zoneid ${JENKINS_ELB_NAME}`
+
+SAMSARA_ELB_NAME=`get_loadbalancer_name samsara`
+SAMSARA_ELB_DNS=`get_loadbalancer_dns samsara`
+SAMSARA_ELB_ZONE_ID=`get_loadbalancer_zoneid ${SAMSARA_ELB_NAME}`
+
+sed "s/%JENKINS_ELB_DNS%/${JENKINS_ELB_DNS}/g" "template_${JENKINS_SAMSARA_DNS_RECORDS_FILE}" |
+    sed "s/%JENKINS_ELB_ZONE_ID%/${JENKINS_ELB_ZONE_ID}/g" |
+    sed "s/%SAMSARA_ELB_DNS%/${SAMSARA_ELB_DNS}/g" |
+    sed "s/%SAMSARA_ELB_ZONE_ID%/${SAMSARA_ELB_ZONE_ID}/g" > ${JENKINS_SAMSARA_DNS_RECORDS_FILE}
+
+aws route53 change-resource-record-sets --hosted-zone-id ${HOSTED_ZONE_ID} --change-batch "file://${JENKINS_SAMSARA_DNS_RECORDS_FILE}"
+# aws route53 get-change --id <place-change-id-here>
