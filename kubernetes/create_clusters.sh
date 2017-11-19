ee48668ea401b2eadfe3cfc25a52ed452dd3a289
lerkasan <lerkasan@gmail.com>
2017-11-19 03:29:27 +0200

    experiments with registry

--- kubernetes/create_clusters.sh
+++ kubernetes/create_clusters.sh
@@ -6,12 +6,13 @@ export AWS_DEFAULT_REGION="us-west-2"
 
 function create_cluster {
     NAME=$1
-    export CLUSTER_NAME="${NAME}-cluster.k8s.local"
+#    export CLUSTER_NAME="${NAME}-cluster.k8s.local"
+    export CLUSTER_NAME="${NAME}.lerkasan.de"
     export KOPS_STATE_STORE="s3://${NAME}-cluster-state"
     echo $CLUSTER_NAME
     echo $KOPS_STATE_STORE
 
-    kops create cluster --zones us-west-2a ${CLUSTER_NAME} # --master-size=t2.medium--node-size=t2.medium \
+    kops create cluster --zones us-west-2a ${CLUSTER_NAME} # --node-count 1 --master-size=t2.medium --node-size=t2.medium \
                                                            # --master-volume-size=20 --node-volume-size=20
 #    kops create -f "./${NAME}-cluster.yaml"
     kops create secret --name ${CLUSTER_NAME} sshpublickey admin -i ~/.ssh/id_rsa.pub
@@ -40,24 +41,26 @@ function create_cluster {
 
 create_cluster jenkins
 kubectl apply -f "registry-deployment.yaml"
+REGISTRY_URL=registry.lerkasan.de
 
 # REGISTRY_URL="`kubectl describe svc registry | grep Ingress | awk '{print $3}'`:5000"
-REGISTRY_URL=`kubectl describe svc registry | grep Ingress | awk '{print $3}'`
-sed -i "s/{{registry_url}}/${REGISTRY_URL}/g" *.yaml
+## REGISTRY_URL=`kubectl describe svc registry | grep Ingress | awk '{print $3}'`
+## sed -i "s/{{registry_url}}/${REGISTRY_URL}/g" *.yaml
+## kubectl apply -f "registry-deployment.yaml"
 
-docker build -t jenkins-slave:latest -f jenkins/Dockerfile.jenkins_slave .
+docker build -t jenkins-slave:latest -f jenkins/Dockerfile.jenkins_slave jenkins
 docker tag jenkins-slave:latest "${REGISTRY_URL}/jenkins-slave:latest"
 docker push "${REGISTRY_URL}/jenkins-slave:latest"
-
-docker build -t jenkins-master:latest -f jenkins/Dockerfile.jenkins_master .
-docker tag jenkins-master:latest "${REGISTRY_URL}/jenkins-master:latest"
-docker push "${REGISTRY_URL}/jenkins-master:latest"
-
-kubectl apply -f "jenkins-deployment.yaml"
-aws iam  attach-role-policy --role-name nodes.jenkins-cluster.k8s.local --policy-arn arn:aws:iam::370535134506:policy/jenkins-nodes-kops
-
-create_cluster samsara
-kubectl create secret generic dbuser-pass --from-literal=password=mysecretpassword
-kubectl apply -f "database-service.yaml"
-kubectl apply -f "samsara-service.yaml"
-kubectl apply -f "samsara-deployment.yaml"
+#
+#docker build -t jenkins-master:latest -f jenkins/Dockerfile.jenkins_master jenkins
+#docker tag jenkins-master:latest "${REGISTRY_URL}/jenkins-master:latest"
+#docker push "${REGISTRY_URL}/jenkins-master:latest"
+#
+#kubectl apply -f "jenkins-deployment.yaml"
+#aws iam  attach-role-policy --role-name nodes.jenkins-cluster.k8s.local --policy-arn arn:aws:iam::370535134506:policy/jenkins-nodes-kops
+#
+#create_cluster samsara
+#kubectl create secret generic dbuser-pass --from-literal=password=mysecretpassword
+#kubectl apply -f "database-service.yaml"
+#kubectl apply -f "samsara-service.yaml"
+#kubectl apply -f "samsara-deployment.yaml"
